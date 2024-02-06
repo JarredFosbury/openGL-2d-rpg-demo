d2d6faeddf6e6bdf2cb4c46e28229a1c2fc51579
cr0barsCorner <58707795+JarredFosbury@users.noreply.github.com>
2024-02-05 22:33:09 -0500

    Added class Shader.java

---
+++ src/shaders/Shader.java
@@ -0,0 +1,78 @@
+package shaders;
+
+import org.joml.*;
+import org.lwjgl.BufferUtils;
+
+import java.nio.FloatBuffer;
+
+import static org.lwjgl.opengl.GL20.*;
+
+public class Shader
+{
+    private int shaderProgramID;
+    private String filepath;
+
+    public int getShaderProgramID()
+    {
+        return shaderProgramID;
+    }
+
+    public String getFilepath()
+    {
+        return filepath;
+    }
+
+    public Shader(String filepath)
+    {
+        this.filepath = filepath;
+        shaderProgramID = ShaderCompiler.compileShader(filepath);
+    }
+
+    public void bind()
+    {
+        glUseProgram(shaderProgramID);
+    }
+
+    public void unbind()
+    {
+        glUseProgram(0);
+    }
+
+    public void updateUniforms()
+    {}
+
+    //TODO: cache these glGetUniformLocation calls somehow, pulling information from the GPU is expensive!!!
+    private void setIntegerUniform(String name, int value)
+    {
+        glUniform1i(glGetUniformLocation(shaderProgramID, name), value);
+    }
+
+    private void setFloatUniform(String name, float value)
+    {
+        glUniform1f(glGetUniformLocation(shaderProgramID, name), value);
+    }
+
+    private void setFloat2Uniform(String name, Vector2f value)
+    {
+        glUniform2f(glGetUniformLocation(shaderProgramID, name), value.x, value.y);
+    }
+
+    private void setFloat3Uniform(String name, Vector3f value)
+    {
+        glUniform3f(glGetUniformLocation(shaderProgramID, name), value.x, value.y, value.z);
+    }
+
+    private void setFloat4Uniform(String name, Vector4f value)
+    {
+        glUniform4f(glGetUniformLocation(shaderProgramID, name), value.x, value.y, value.z, value.w);
+    }
+
+    private void setMatrix4Uniform(String name, Matrix4f value)
+    {
+        // TODO: Verify that the matrix orientation is indeed transpose!
+
+        FloatBuffer matrixValuesBuffer = BufferUtils.createFloatBuffer(16);
+        value.get(matrixValuesBuffer);
+        glUniformMatrix4fv(glGetUniformLocation(shaderProgramID, name), false, matrixValuesBuffer);
+    }
+}
