ce96b1509d35dcbca73fdd048ac471415976a58a
cr0barsCorner <58707795+JarredFosbury@users.noreply.github.com>
2024-02-06 14:42:37 -0500

    Updated shader to support texture mapping

--- res/shaders/ScreenSpace2D.glsl
+++ res/shaders/ScreenSpace2D.glsl
@@ -1,19 +1,26 @@
 #type vertex
 #version 330 core
 layout (location = 0) in vec3 aPos;
+layout (location = 1) in vec2 aTexPos;
+
+out vec2 TexPos;
 
 void main()
 {
     gl_Position = vec4(aPos.xyz, 1.0);
+    TexPos = aTexPos;
 }
 
 #type fragment
 #version 330 core
 out vec4 FragColor;
 
+in vec2 TexPos;
+
 uniform vec4 tint;
+uniform sampler2D textureMain;
 
 void main()
 {
-    FragColor = vec4(1.0, 1.0, 1.0, 1.0) * tint;
+    FragColor = texture(textureMain, TexPos) * tint;
 }
