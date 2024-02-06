30dda3269873b30688250cbb4a2f09ea11f25787
cr0barsCorner <58707795+JarredFosbury@users.noreply.github.com>
2024-02-05 23:57:25 -0500

    First render

--- src/core/Scene.java
+++ src/core/Scene.java
@@ -1,9 +1,48 @@
 package core;
 
+import shaders.Shader;
+
+import static org.lwjgl.opengl.GL15.*;
+import static org.lwjgl.opengl.GL30.*;
+
 public class Scene
 {
+    private static final float[] vertices = {
+            0.5f,  0.5f, 0.0f,  // top right
+            0.5f, -0.5f, 0.0f,  // bottom right
+            -0.5f, -0.5f, 0.0f,  // bottom left
+            -0.5f,  0.5f, 0.0f   // top left
+    };
+
+    private static final int[] indices = {
+            0, 1, 3,   // first triangle
+            1, 2, 3    // second triangle
+    };
+
+    private static int vboID, vaoID, eboID;
+    private static Shader screenSpace2d_shader;
+
     public static void initialize()
-    {}
+    {
+        vboID = glGenBuffers();
+        vaoID = glGenVertexArrays();
+        eboID = glGenBuffers();
+
+        glBindVertexArray(vaoID);
+
+        glBindBuffer(GL_ARRAY_BUFFER, vboID);
+        glBufferData(GL_ARRAY_BUFFER, vertices, GL_STATIC_DRAW);
+
+        glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, eboID);
+        glBufferData(GL_ELEMENT_ARRAY_BUFFER, indices, GL_STATIC_DRAW);
+
+        glVertexAttribPointer(0, 3, GL_FLOAT, false, 3 * Float.BYTES, 0);
+        glEnableVertexAttribArray(0);
+
+        screenSpace2d_shader = new Shader("./res/shaders/ScreenSpace2D.glsl");
+
+        glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
+    }
 
     public static void pollInput()
     {}
@@ -15,5 +54,11 @@ public class Scene
     {}
 
     public static void render()
-    {}
+    {
+        screenSpace2d_shader.bind();
+        glBindVertexArray(vaoID);
+        glDrawElements(GL_TRIANGLES, indices.length, GL_UNSIGNED_INT, 0);
+        glBindVertexArray(0);
+        screenSpace2d_shader.unbind();
+    }
 }
