e66d536d62ec0aea44454fb009af9348854ec1b3
cr0barsCorner <58707795+JarredFosbury@users.noreply.github.com>
2024-02-07 20:25:07 -0500

    Added class Color.java

---
+++ src/rendering/Color.java
@@ -0,0 +1,12 @@
+package rendering;
+
+import org.joml.Vector4f;
+
+public class Color
+{
+    public final static Vector4f WHITE  = new Vector4f(1.0f, 1.0f, 1.0f, 1.0f);
+    public final static Vector4f BLACK  = new Vector4f(0.0f, 0.0f, 0.0f, 1.0f);
+    public final static Vector4f RED    = new Vector4f(1.0f, 0.0f, 0.0f, 1.0f);
+    public final static Vector4f GREEN  = new Vector4f(0.0f, 1.0f, 0.0f, 1.0f);
+    public final static Vector4f BLUE   = new Vector4f(0.0f, 0.0f, 1.0f, 1.0f);
+}
