fa16a7d2ce768b5b44374673114f586a63f7ea38
cr0barsCorner <58707795+JarredFosbury@users.noreply.github.com>
2024-02-07 23:26:09 -0500

    Added method for manipulating viewport values at runtime

--- src/core/Camera.java
+++ src/core/Camera.java
@@ -9,16 +9,28 @@ public class Camera
     public Matrix4f screenSpaceProjection;
     public Vector3f position;
     public Vector3f rotation;
+    public float viewPlaneScale;
+    public float nearPlane;
+    public float farPlane;
 
     public Camera()
     {
-        float halfWidth = (float)GlobalSettings.WINDOW_WIDTH / 2.0f;
-        float halfHeight = (float)GlobalSettings.WINDOW_HEIGHT / 2.0f;
-        float aspectRatio = halfWidth / halfHeight;
-        projection = new Matrix4f().ortho(-aspectRatio, aspectRatio, -1.0f, 1.0f, -1.0f, 1.0f);
-        screenSpaceProjection = new Matrix4f().ortho(-halfWidth, halfWidth, -halfHeight, halfHeight, -1.0f, 1.0f);
+        updateViewport(1.0f, -1.0f, 1.0f);
         position = new Vector3f(0.0f, 0.0f, 0.0f);
         rotation = new Vector3f(0.0f, 0.0f, 0.0f);
+    }
+
+    public void updateViewport(float viewPlaneScale, float nearPlane, float farPlane)
+    {
+        this.viewPlaneScale = viewPlaneScale;
+        this.nearPlane = nearPlane;
+        this.farPlane = farPlane;
+
+        float halfWidth = (float)GlobalSettings.WINDOW_WIDTH / 2.0f;
+        float halfHeight = (float)GlobalSettings.WINDOW_HEIGHT / 2.0f;
+        float aspectRatio = (halfWidth / halfHeight) * viewPlaneScale;
+        projection = new Matrix4f().ortho(-aspectRatio, aspectRatio, -1.0f * viewPlaneScale, viewPlaneScale, nearPlane, farPlane);
+        screenSpaceProjection = new Matrix4f().ortho(-halfWidth, halfWidth, -halfHeight, halfHeight, -1.0f, 1.0f);
     }
 
     public Matrix4f getTransformation()
--- src/core/Scene.java
+++ src/core/Scene.java
@@ -1,6 +1,5 @@
 package core;
 
-import org.joml.Vector3f;
 import rendering.Color;
 import rendering.Sprite;
 
@@ -12,8 +11,6 @@ public class Scene
     public static Camera mainCamera;
 
     private static Sprite brickSprite;
-    private static int horizontalAxis;
-    private static int verticalAxis;
     public static void initialize()
     {
         glPolygonMode(GL_FRONT_FACE, GL_FILL);
@@ -25,30 +22,13 @@ public class Scene
     }
 
     public static void pollInput()
-    {
-        if (KeyListener.isKeyActive(GLFW_KEY_W))
-            verticalAxis = 1;
-        else if (KeyListener.isKeyActive(GLFW_KEY_S))
-            verticalAxis = -1;
-        else
-            verticalAxis = 0;
-
-        if (KeyListener.isKeyActive(GLFW_KEY_D))
-            horizontalAxis = 1;
-        else if (KeyListener.isKeyActive(GLFW_KEY_A))
-            horizontalAxis = -1;
-        else
-            horizontalAxis = 0;
-    }
+    {}
 
     public static void update()
     {}
 
     public static void fixedPhysicsUpdate()
-    {
-        brickSprite.rotate(0.0f, 0.0f, 45.0f * Time.fixedPhysicsTimeStep);
-        mainCamera.translate(horizontalAxis * 2.5f * Time.fixedPhysicsTimeStep, verticalAxis * 2.5f * Time.fixedPhysicsTimeStep, 0.0f);
-    }
+    {}
 
     public static void render()
     {
