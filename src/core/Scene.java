41a699e64c5a2e035cbda39fbaaa8cb0ac493bb6
cr0barsCorner <58707795+JarredFosbury@users.noreply.github.com>
2024-02-07 19:59:41 -0500

    Added class ScreenSpaceSprite.java

--- src/core/Scene.java
+++ src/core/Scene.java
@@ -1,67 +1,20 @@
 package core;
 
-import org.joml.Matrix4f;
-import org.joml.Vector3f;
-import org.joml.Vector4f;
-import rendering.Texture;
-import shaders.ScreenSpace2dShader;
+import rendering.ScreenSpaceSprite;
 
 import static org.lwjgl.opengl.GL15.*;
-import static org.lwjgl.opengl.GL30.*;
 
 public class Scene
 {
-    private static final float[] vertices = {
-            // positions            // texture coordinates
-            0.5f,  0.5f, 0.0f,      1.0f, 1.0f,     // top right
-            0.5f, -0.5f, 0.0f,      1.0f, 0.0f,     // bottom right
-            -0.5f, -0.5f, 0.0f,     0.0f, 0.0f,     // bottom left
-            -0.5f,  0.5f, 0.0f,     0.0f, 1.0f      // top left
-    };
-
-    private static final int[] indices = {
-            0, 1, 3,   // first triangle
-            1, 2, 3    // second triangle
-    };
-
-    private static int vboID, vaoID, eboID;
-    private static ScreenSpace2dShader screenSpace2d_shader;
-    private static Texture bricks;
-    private static Matrix4f transform;
-    private static Matrix4f projection;
+    private static ScreenSpaceSprite heartSprite;
 
     public static void initialize()
     {
-        vboID = glGenBuffers();
-        vaoID = glGenVertexArrays();
-        eboID = glGenBuffers();
-
-        glBindVertexArray(vaoID);
-
-        glBindBuffer(GL_ARRAY_BUFFER, vboID);
-        glBufferData(GL_ARRAY_BUFFER, vertices, GL_STATIC_DRAW);
-
-        glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, eboID);
-        glBufferData(GL_ELEMENT_ARRAY_BUFFER, indices, GL_STATIC_DRAW);
-
-        glVertexAttribPointer(0, 3, GL_FLOAT, false, 5 * Float.BYTES, 0);
-        glEnableVertexAttribArray(0);
-
-        glVertexAttribPointer(1, 2, GL_FLOAT, false, 5 * Float.BYTES, 3 * Float.BYTES);
-        glEnableVertexAttribArray(1);
-
         glPolygonMode(GL_FRONT_FACE, GL_FILL);
+        glEnable(GL_BLEND);
+        glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
 
-        screenSpace2d_shader = new ScreenSpace2dShader("./res/shaders/ScreenSpace2D.glsl");
-        bricks = new Texture("./res/textures/bricks_01.jpg", true);
-
-        transform = new Matrix4f().identity();
-        transform.translate(new Vector3f(0.0f, 0.0f, 0.0f));
-        transform.rotateXYZ(new Vector3f(0.0f, 0.0f, 0.0f));
-        transform.scale(new Vector3f(1, 1, 1));
-
-        float aspectRatio = ((float)GlobalSettings.WINDOW_WIDTH / (float)GlobalSettings.WINDOW_HEIGHT);
-        projection = new Matrix4f().ortho(-aspectRatio, aspectRatio, -1.0f, 1.0f, -1.0f, 1.0f);
+        heartSprite = new ScreenSpaceSprite("./res/textures/UI/heart.png");
     }
 
     public static void pollInput()
@@ -71,18 +24,10 @@ public class Scene
     {}
 
     public static void fixedPhysicsUpdate()
-    {
-        transform.rotateXYZ(0.0f, 0.0f, (float)Math.toRadians(45.0) * GlobalSettings.fixedPhysicsTimeStep);
-    }
+    {}
 
     public static void render()
     {
-        screenSpace2d_shader.bind();
-        bricks.bind(0);
-        screenSpace2d_shader.updateUniforms(new Vector4f(1.0f, 1.0f, 1.0f, 1.0f), transform, projection);
-        glBindVertexArray(vaoID);
-        glDrawElements(GL_TRIANGLES, indices.length, GL_UNSIGNED_INT, 0);
-        glBindVertexArray(0);
-        screenSpace2d_shader.unbind();
+        heartSprite.render();
     }
 }
---
+++ src/rendering/ScreenSpaceSprite.java
@@ -0,0 +1,79 @@
+package rendering;
+
+import core.GlobalSettings;
+import org.joml.*;
+import shaders.ScreenSpace2dShader;
+
+import static org.lwjgl.opengl.GL11.GL_FLOAT;
+import static org.lwjgl.opengl.GL15.*;
+import static org.lwjgl.opengl.GL20.*;
+import static org.lwjgl.opengl.GL30.*;
+
+public class ScreenSpaceSprite
+{
+    private final float[] vertexData = {
+            // positions            // texture coordinates
+            0.5f,  0.5f, 0.0f,      1.0f, 1.0f,     // top right
+            0.5f, -0.5f, 0.0f,      1.0f, 0.0f,     // bottom right
+            -0.5f, -0.5f, 0.0f,     0.0f, 0.0f,     // bottom left
+            -0.5f,  0.5f, 0.0f,     0.0f, 1.0f      // top left
+    };
+
+    private final int[] indices = {
+            0, 1, 3,   // first triangle
+            1, 2, 3    // second triangle
+    };
+
+    private final int vboID, vaoID, eboID;
+    private final ScreenSpace2dShader screenSpace2d_shader;
+    private Texture mainTexture;
+    private Matrix4f screenSpaceProjection;
+
+    public Vector3f position;
+    public Vector3f rotation;
+    public Vector3f scale;
+
+    public ScreenSpaceSprite(String mainTexture)
+    {
+        this.mainTexture = new Texture(mainTexture, true);
+        screenSpace2d_shader = new ScreenSpace2dShader("./res/shaders/ScreenSpace2D.glsl");
+        vboID = glGenBuffers();
+        vaoID = glGenVertexArrays();
+        eboID = glGenBuffers();
+
+        glBindVertexArray(vaoID);
+        glBindBuffer(GL_ARRAY_BUFFER, vboID);
+        glBufferData(GL_ARRAY_BUFFER, vertexData, GL_STATIC_DRAW);
+        glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, eboID);
+        glBufferData(GL_ELEMENT_ARRAY_BUFFER, indices, GL_STATIC_DRAW);
+        glVertexAttribPointer(0, 3, GL_FLOAT, false, 5 * Float.BYTES, 0);
+        glEnableVertexAttribArray(0);
+        glVertexAttribPointer(1, 2, GL_FLOAT, false, 5 * Float.BYTES, 3 * Float.BYTES);
+        glEnableVertexAttribArray(1);
+
+        position = new Vector3f(0.0f, 0.0f, 0.0f);
+        rotation = new Vector3f(0.0f, 0.0f, 0.0f);
+        scale = new Vector3f(100.0f, 100.0f, 100.0f);
+
+        // TODO: transfer this to the active scene camera
+        float halfWidth = (float)GlobalSettings.WINDOW_WIDTH / 2.0f;
+        float halfHeight = (float)GlobalSettings.WINDOW_HEIGHT / 2.0f;
+        screenSpaceProjection = new Matrix4f().ortho(-halfWidth, halfWidth, -halfHeight, halfHeight, -1.0f, 1.0f);
+    }
+
+    public void render()
+    {
+        Matrix4f transform = new Matrix4f().identity();
+        transform.translate(position);
+        transform.rotateXYZ(rotation);
+        transform.scale(scale);
+
+        screenSpace2d_shader.bind();
+        mainTexture.bind(0);
+        screenSpace2d_shader.updateUniforms(new Vector4f(0.812f, 0.176f, 0.22f, 1.0f), transform, screenSpaceProjection);
+        glBindVertexArray(vaoID);
+        glDrawElements(GL_TRIANGLES, indices.length, GL_UNSIGNED_INT, 0);
+        glBindVertexArray(0);
+        screenSpace2d_shader.unbind();
+    }
+}
