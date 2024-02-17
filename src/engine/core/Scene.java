f390b676688976b7c8bfe794112a65415612678f
cr0barsCorner <58707795+JarredFosbury@users.noreply.github.com>
2024-02-17 02:13:41 -0500

    Added campfire to scene

--- src/engine/core/Scene.java
+++ src/engine/core/Scene.java
@@ -1,15 +1,12 @@
 package engine.core;
 
-import engine.fontRendering.FontLoader;
+import engine.fontRendering.*;
 import engine.rendering.*;
-import engine.shaders.ScreenSpace2dShader;
-import engine.shaders.Standard2dShader;
-import game.DayNightCycle;
-import game.PlayerSprite;
-import org.joml.Vector2f;
-import org.joml.Vector2i;
-import org.joml.Vector3f;
+import engine.shaders.*;
+import game.*;
+import org.joml.*;
 
+import static org.lwjgl.glfw.GLFW.*;
 import static org.lwjgl.opengl.GL15.*;
 
 public class Scene
@@ -22,7 +19,10 @@ public class Scene
     public static DayNightCycle dayNightCycle;
     public static PlayerSprite player;
     public static Sprite groundPlane;
-    public static TextMesh xp_UI;
+    public static Sprite campfirePit;
+    public static Sprite fire;
+
+    public static TextMesh time_UI;
 
     public static void initialize()
     {
@@ -37,20 +37,32 @@ public class Scene
         screenSpace2dShader = new ScreenSpace2dShader("res/shaders/ScreenSpace2D.glsl");
 
         dayNightCycle = new DayNightCycle();
-        dayNightCycle.totalCycleLength_MINS = 20.0f;
-        dayNightCycle.setTime(9, 0);
+        dayNightCycle.totalCycleLength_MINS = 25.0f;
+        dayNightCycle.setTime(19, 25);
         dayNightCycle.mainLightColor = dayNightCycle.dayLightColor;
 
         player = new PlayerSprite();
+
         groundPlane = new Sprite(new Texture("res/textures/SizeBasedGrids/c2m.png", true, true, false),
                 dayNightCycle.mainLightColor, new Vector2f(0.0f), new Vector2f(10.0f));
         groundPlane.scale = new Vector3f(20.0f);
 
-        xp_UI = new TextMesh(FontLoader.loadFont("res/fonts/liberation mono/LiberationMono_512x512_glyphMap.png",
+        campfirePit = new Sprite(new Texture("res/textures/Tilesets/001-Grassland01.png", true, false, true),
+                dayNightCycle.mainLightColor, new Vector2f(0.6328125f, 0.78298611f), new Vector2f(0.1f, 0.05f));
+
+        fire = new Sprite(new Texture("res/textures/fireSheet_lowRes.png", true, false, true),
+                dayNightCycle.mainLightColor, new Vector2f(0.0f, 0.75f), new Vector2f(0.125f, 0.25f));
+        fire.scale = new Vector3f(1.0f, 2.0f, 1.0f);
+        fire.position = new Vector3f(0.0f, 0.65f, 0.0f);
+        fire.initSpriteSheet("res/spriteSheetData/fireSheetData.ssd");
+        fire.animationFrameRate = 30;
+
+        time_UI = new TextMesh(FontLoader.loadFont("res/fonts/liberation mono/LiberationMono_512x512_glyphMap.png",
                 "res/fonts/liberation mono/LiberationMono_512x512_glyphMap.fnt"), true);
-        xp_UI.fontSize_PIXELS = 32;
-        xp_UI.locationAnchor = new Vector2i(-1, 1);
-        xp_UI.position = new Vector3f(20.0f, -50.0f, 0.0f);
+        time_UI.fontSize_PIXELS = 24;
+        time_UI.locationAnchor = new Vector2i(1, 1);
+        time_UI.position = new Vector3f(-25, -25, 0.0f);
+        time_UI.textAlignment = new Vector2i(-1, 0);
     }
 
     public static void pollInput()
@@ -64,6 +76,8 @@ public class Scene
         player.update();
         player.updateColor(dayNightCycle.mainLightColor);
         groundPlane.mainTextureTint = dayNightCycle.mainLightColor;
+        campfirePit.mainTextureTint = dayNightCycle.mainLightColor;
+        fire.animateSprite();
     }
 
     public static void fixedPhysicsUpdate()
@@ -75,8 +89,10 @@ public class Scene
     public static void render()
     {
         groundPlane.render();
+        campfirePit.render();
+        fire.render();
         player.render();
 
-        xp_UI.drawString((int) dayNightCycle.timeInGame_HOURS + " HRS, " + (int) dayNightCycle.timeInGame_MINS + " MINS", Color.BLUE);
+        time_UI.drawString("Time: " + (int) dayNightCycle.timeInGame_HOURS + " hrs, " + (int) dayNightCycle.timeInGame_MINS + " mins", Color.WHITE);
     }
 }
