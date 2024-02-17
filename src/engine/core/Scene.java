859ee86fbd9ca2e1a801901e078b539e8d1476c9
cr0barsCorner <58707795+JarredFosbury@users.noreply.github.com>
2024-02-17 18:12:03 -0500

    Added tree, stump. and 2 new pieces of UI

--- src/engine/core/Scene.java
+++ src/engine/core/Scene.java
@@ -21,11 +21,15 @@ public class Scene
 
     public static DayNightCycle dayNightCycle;
     public static PlayerSprite player;
-    public static Sprite groundPlane;
+    public static IslandEnvironmentSprites island;
     public static Sprite campfirePit;
     public static Sprite fire;
+    public static Sprite tree;
+    public static Sprite treeStump;
 
     public static TextMesh time_UI;
+    public static TextMesh hp_UI;
+    public static TextMesh xp_UI;
 
     public static ColorPickerWindow colorPickerWindow;
 
@@ -50,19 +54,27 @@ public class Scene
 
         player = new PlayerSprite();
 
-        groundPlane = new Sprite(new Texture("res/textures/SizeBasedGrids/c2m.png", true, true, false),
-                dayNightCycle.mainLightColor, new Vector2f(0.0f), new Vector2f(10.0f));
-        groundPlane.scale = new Vector3f(20.0f);
+        island = new IslandEnvironmentSprites();
 
-        campfirePit = new Sprite(new Texture("res/textures/Tilesets/001-Grassland01.png", true, false, true),
-                dayNightCycle.mainLightColor, new Vector2f(0.6328125f, 0.78298611f), new Vector2f(0.1f, 0.05f));
+        campfirePit = new Sprite(new Texture("res/textures/campfire.png", true, false, true),
+                dayNightCycle.mainLightColor, new Vector2f(0.0f), new Vector2f(1.0f));
 
         fire = new Sprite(new Texture("res/textures/fireSheet_lowRes.png", true, false, true),
                 dayNightCycle.mainLightColor, new Vector2f(0.0f, 0.75f), new Vector2f(0.125f, 0.25f));
         fire.scale = new Vector3f(1.0f, 2.0f, 1.0f);
-        fire.position = new Vector3f(0.0f, 0.65f, 0.0f);
+        fire.position = new Vector3f(0.0f, 0.75f, 0.0f);
         fire.initSpriteSheet("res/spriteSheetData/fireSheetData.ssd");
         fire.animationFrameRate = 30;
+
+        tree = new Sprite(new Texture("res/textures/tree_01.png", true, true, true),
+                dayNightCycle.mainLightColor, new Vector2f(0.0f), new Vector2f(1.0f));
+        tree.scale = new Vector3f(3.0f, 4.0f, 1.0f);
+        tree.translate(0.0f, 4.0f, 0.0f);
+
+        treeStump = new Sprite(new Texture("res/textures/treeStump_01.png", true, true, true),
+                dayNightCycle.mainLightColor, new Vector2f(0.0f), new Vector2f(1.0f));
+        treeStump.scale = new Vector3f(0.75f, 1.0f, 1.0f);
+        treeStump.translate(0.0f, 2.3f, 0.0f);
 
         time_UI = new TextMesh(FontLoader.loadFont("res/fonts/press start/press-start_512x512_glyphMap.png",
                 "res/fonts/press start/press-start_512x512_glyphMap.fnt"), true);
@@ -70,6 +82,18 @@ public class Scene
         time_UI.locationAnchor = new Vector2i(1, 1);
         time_UI.position = new Vector3f(-25, -25, 0.0f);
         time_UI.textAlignment = new Vector2i(-1, 0);
+
+        hp_UI = new TextMesh(FontLoader.loadFont("res/fonts/press start/press-start_512x512_glyphMap.png",
+                "res/fonts/press start/press-start_512x512_glyphMap.fnt"), true);
+        hp_UI.fontSize_PIXELS = 45;
+        hp_UI.locationAnchor = new Vector2i(-1, 1);
+        hp_UI.position = new Vector3f(25, -50, 0.0f);
+
+        xp_UI = new TextMesh(FontLoader.loadFont("res/fonts/press start/press-start_512x512_glyphMap.png",
+                "res/fonts/press start/press-start_512x512_glyphMap.fnt"), true);
+        xp_UI.fontSize_PIXELS = 45;
+        xp_UI.locationAnchor = new Vector2i(-1, 1);
+        xp_UI.position = new Vector3f(25, -100, 0.0f);
 
         colorPickerWindow = new ColorPickerWindow();
     }
@@ -84,9 +108,11 @@ public class Scene
         dayNightCycle.update();
         player.update();
         player.updateColor(dayNightCycle.mainLightColor);
-        groundPlane.mainTextureTint = dayNightCycle.mainLightColor;
         campfirePit.mainTextureTint = dayNightCycle.mainLightColor;
+        tree.mainTextureTint = dayNightCycle.mainLightColor;
+        treeStump.mainTextureTint = dayNightCycle.mainLightColor;
         fire.animateSprite();
+        island.update();
     }
 
     public static void fixedPhysicsUpdate()
@@ -97,12 +123,18 @@ public class Scene
 
     public static void render()
     {
-        groundPlane.render();
+        island.render();
         campfirePit.render();
-        fire.render();
+        treeStump.render();
+
         player.render();
 
-        time_UI.drawString("Time " + dayNightCycle.getTimeFormatted(), Color.BLACK);
+        fire.render();
+        tree.render();
+
+        time_UI.drawString("Time " + dayNightCycle.getTimeFormatted(), new Vector4f(0.902f, 0.902f, 0.902f, 1.0f));
+        hp_UI.drawString("HP 80/80", new Vector4f(0.814f, 0.032f, 0.032f, 1.0f));
+        xp_UI.drawString("XP 0/50", new Vector4f(0.637f, 0.128f, 0.532f, 1.0f));
     }
 
     public static void renderImGui()
