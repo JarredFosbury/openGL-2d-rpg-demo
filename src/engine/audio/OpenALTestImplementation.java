3bdfdf279aac3171c46f9ca4e8366a2b6453ba24
cr0barsCorner <58707795+JarredFosbury@users.noreply.github.com>
2024-03-01 15:12:36 -0500

    Setup methods for initializing and cleaning up OpenAL context and devices

--- src/engine/audio/OpenALTestImplementation.java
+++ src/engine/audio/OpenALTestImplementation.java
@@ -1,9 +1,41 @@
 package engine.audio;
 
+import org.lwjgl.openal.AL;
+import org.lwjgl.openal.ALC;
+import org.lwjgl.openal.ALCCapabilities;
+import org.lwjgl.openal.ALCapabilities;
+
+import static org.lwjgl.openal.ALC10.*;
+
 public class OpenALTestImplementation
 {
+    private static long audioContext;
+    private static long audioDevice;
+
     public static void main(String[] args)
     {
-        System.out.println("Hello world!");
+        initOpenAL();
+    }
+
+    private static void initOpenAL()
+    {
+        String defaultDeviceName = alcGetString(0, ALC_DEFAULT_DEVICE_SPECIFIER);
+        audioDevice = alcOpenDevice(defaultDeviceName);
+
+        int[] attributes = {0};
+        audioContext = alcCreateContext(audioContext, attributes);
+        alcMakeContextCurrent(audioContext);
+
+        ALCCapabilities alcCapabilities = ALC.createCapabilities(audioDevice);
+        ALCapabilities alCapabilities = AL.createCapabilities(alcCapabilities);
+
+        if (!alCapabilities.OpenAL10)
+            assert false : "Audio library not supported!";
+    }
+
+    private static void cleanUpOpenAL()
+    {
+        alcDestroyContext(audioContext);
+        alcCloseDevice(audioDevice);
     }
 }
