199484ef14c8a4c770fd333d6ef6807a7f8c26ab
cr0barsCorner <58707795+JarredFosbury@users.noreply.github.com>
2024-03-03 18:24:24 -0500

    Updated Entity.java to use types and names

--- src/engine/core/Entity.java
+++ src/engine/core/Entity.java
@@ -4,17 +4,23 @@ import org.joml.Vector3f;
 
 public class Entity
 {
+    public String name;
+    public final EntityType TYPE;
     public Vector3f position;
     public Vector3f rotation;
     public Vector3f scale;
 
-    public Entity()
+    public Entity(String name, EntityType type)
     {
         Scene.entityList.add(this);
-
+        this.name = name;
+        this.TYPE = type;
         position = new Vector3f(0.0f);
         rotation = new Vector3f(0.0f);
         scale = new Vector3f(1.0f);
+
+        if (TYPE == EntityType.NULL)
+            System.err.println("WARNING: Class type set to NULL!");
     }
 
     public void pollInput()
@@ -41,12 +47,12 @@ public class Entity
 
     public void rotate(float x, float y, float z)
     {
-        rotation.add(x, y, z);
+        rotation.add((float) Math.toRadians(x), (float)Math.toRadians(y), (float)Math.toRadians(z));
     }
 
     public void rotate(Vector3f v)
     {
-        rotation.add(v.x, v.y, v.z);
+        rotation.add((float) Math.toRadians(v.x), (float)Math.toRadians(v.y), (float)Math.toRadians(v.z));
     }
 
     public void scale(float x, float y, float z)
