e13012982c6b5b71d17f99df78f612d80169cb96
cr0barsCorner <58707795+JarredFosbury@users.noreply.github.com>
2024-03-08 16:13:32 -0500

    Created new package templates for example classes and added a new class template ScriptableBehaviour

---
+++ src/templates/ScriptableBehaviour.java
@@ -0,0 +1,24 @@
+package templates;
+
+import engine.core.Entity;
+import engine.core.EntityType;
+
+public class ScriptableBehaviour extends Entity
+{
+    public ScriptableBehaviour()
+    {
+        super("Entity name here", EntityType.ScriptableBehavior);
+    }
+
+    public void pollInput()
+    {}
+
+    public void update()
+    {}
+
+    public void fixedPhysicsUpdate()
+    {}
+
+    public void render()
+    {}
+}
