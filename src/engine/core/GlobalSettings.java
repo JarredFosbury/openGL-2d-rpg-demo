079c08da448ee08eed3ce48ffd97a5579de3623e
cr0barsCorner <58707795+JarredFosbury@users.noreply.github.com>
2024-03-10 04:12:11 -0400

    Updated default values

--- src/engine/core/GlobalSettings.java
+++ src/engine/core/GlobalSettings.java
@@ -5,11 +5,11 @@ public class GlobalSettings
     public final static String APPLICATION_NAME = "OpenGL 2d RPG Demo";
     public final static String GLSL_VERSION     = "#version 330";
 
-    public static int windowWidth               = 1920;
-    public static int windowHeight              = 1080;
+    public static int windowWidth               = 2560;
+    public static int windowHeight              = 1411;
     public static boolean useVsync              = true;
     public static boolean useLauncher           = false;
-    public static boolean windowMaximized       = false;
+    public static boolean windowMaximized       = true;
     public static boolean windowBorderless      = false;
     public static boolean canBeResized          = false;
     public static int msaaSamples               = 1;
