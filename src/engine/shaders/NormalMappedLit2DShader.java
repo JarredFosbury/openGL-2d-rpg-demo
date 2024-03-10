5633a7d0e85130f53f2152f294514b35c4bbf493
cr0barsCorner <58707795+JarredFosbury@users.noreply.github.com>
2024-03-10 17:55:04 -0400

    Created class NormalMappedLit2DShader.java

---
+++ src/engine/shaders/NormalMappedLit2DShader.java
@@ -0,0 +1,23 @@
+package engine.shaders;
+
+import org.joml.Matrix4f;
+import org.joml.Vector2f;
+import org.joml.Vector4f;
+
+public class NormalMappedLit2DShader extends Shader
+{
+    public NormalMappedLit2DShader(String filepath)
+    {
+        super(filepath);
+    }
+
+    public void updateUniforms(Vector4f tint, Matrix4f transform, Matrix4f projection, Matrix4f cameraTransform, Vector2f texPosOffset, Vector2f texPosScale)
+    {
+        setFloat4Uniform("tint", tint);
+        setMatrix4Uniform("transform", transform);
+        setMatrix4Uniform("projection", projection);
+        setMatrix4Uniform("cameraTransform", cameraTransform);
+        setFloat2Uniform("texPosOffset", texPosOffset);
+        setFloat2Uniform("texPosScale", texPosScale);
+    }
+}
