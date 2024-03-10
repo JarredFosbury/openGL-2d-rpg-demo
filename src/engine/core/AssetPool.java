db16a25cb3d1a6e559a2492f95926bb093e18d9e
cr0barsCorner <58707795+JarredFosbury@users.noreply.github.com>
2024-03-09 19:31:41 -0500

    Removed shaders and swapped getAllAssetKeys output order to type, key from key, type

--- src/engine/core/AssetPool.java
+++ src/engine/core/AssetPool.java
@@ -3,13 +3,11 @@ package engine.core;
 import engine.audio.SoundClip;
 import engine.fontRendering.Font;
 import engine.rendering.Texture;
-import engine.shaders.Shader;
 
 import java.util.HashMap;
 
 public class AssetPool
 {
-    public HashMap<String, Shader> shaderPool;
     public HashMap<String, Texture> texturePool;
     public HashMap<String, SoundClip> soundClipPool;
     public HashMap<String, Font> fontPool;
@@ -18,7 +16,6 @@ public class AssetPool
 
     public AssetPool()
     {
-        shaderPool = new HashMap<>();
         texturePool = new HashMap<>();
         soundClipPool = new HashMap<>();
         fontPool = new HashMap<>();
@@ -29,9 +26,6 @@ public class AssetPool
     {
         if (isEmpty)
             isEmpty = false;
-
-        if (asset.getClass().equals(Shader.class))
-            shaderPool.put(name, (Shader) asset);
 
         if (asset.getClass().equals(Texture.class))
             texturePool.put(name, (Texture) asset);
@@ -47,13 +41,9 @@ public class AssetPool
     {
         Object assetOut = null;
 
-        Shader shaderOut = shaderPool.get(name);
         Texture textureOut = texturePool.get(name);
         SoundClip soundClipOut = soundClipPool.get(name);
         Font fontOut = fontPool.get(name);
-
-        if (shaderOut != null)
-            assetOut = shaderOut;
 
         if (textureOut != null)
             assetOut = textureOut;
@@ -75,9 +65,6 @@ public class AssetPool
             return;
         }
 
-        for (String key : shaderPool.keySet())
-            shaderPool.get(key).delete();
-
         for (String key : texturePool.keySet())
             texturePool.get(key).delete();
 
@@ -87,7 +74,6 @@ public class AssetPool
         for (String key : fontPool.keySet())
             fontPool.get(key).delete();
 
-        shaderPool.clear();
         texturePool.clear();
         soundClipPool.clear();
         fontPool.clear();
@@ -96,7 +82,7 @@ public class AssetPool
 
     public int getTotalPoolSize()
     {
-        return shaderPool.size() + texturePool.size() + soundClipPool.size() + fontPool.size();
+        return texturePool.size() + soundClipPool.size() + fontPool.size();
     }
 
     public String[] getAllAssetKeys()
@@ -104,27 +90,21 @@ public class AssetPool
         String[] outAssetKeys = new String[getTotalPoolSize()];
         int index = 0;
 
-        for (String key : shaderPool.keySet())
-        {
-            outAssetKeys[index] = key + ", Shader";
-            index ++;
-        }
-
         for (String key : texturePool.keySet())
         {
-            outAssetKeys[index] = key + ", Texture";
+            outAssetKeys[index] = "Texture, " + key;
             index ++;
         }
 
         for (String key : soundClipPool.keySet())
         {
-            outAssetKeys[index] = key + ", SoundClip";
+            outAssetKeys[index] = "SoundClip, " + key;
             index ++;
         }
 
         for (String key : fontPool.keySet())
         {
-            outAssetKeys[index] = key + ", Font";
+            outAssetKeys[index] = "Font, " + key;
             index ++;
         }
 
