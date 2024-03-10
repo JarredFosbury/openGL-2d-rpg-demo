59fc55a502a031b81e20cdfbbcb1576e7876da4b
cr0barsCorner <58707795+JarredFosbury@users.noreply.github.com>
2024-03-09 22:01:57 -0500

    Updated AssetPoolWindow to show loaded assets in scene

--- src/engine/imGui/AssetPoolWindow.java
+++ src/engine/imGui/AssetPoolWindow.java
@@ -3,19 +3,34 @@ package engine.imGui;
 import engine.core.Scene;
 import engine.patternInterfaces.Observable;
 import engine.patternInterfaces.Observer;
+import imgui.ImGui;
 
 public class AssetPoolWindow extends ImGuiWindow implements Observer
 {
+    private short assetCount;
+    private String[] assets;
+
     public AssetPoolWindow()
     {
-        super("asset_pool_window", "Asset Pool Viewer");
+        super("asset_pool_window", "Asset Pool Viewer (0 assets)");
         Scene.assets.addObserver(this);
     }
 
     public void renderWindowContents()
-    {}
+    {
+        setTitle("Asset Pool Viewer (" + assetCount + " assets)");
+
+        for (int i = 0; i < assets.length; i++)
+        {
+            String[] halves = assets[i].split(",");
+            ImGui.selectable("(" + halves[0] + ")" + halves[1]);
+        }
+    }
 
     @Override
     public void update(Observable target)
-    {}
+    {
+        assetCount = Scene.assets.getTotalPoolSize();
+        assets = Scene.assets.getAllAssetKeys();
+    }
 }
