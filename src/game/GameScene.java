d769fa0bd6584f4f1e2f822ccb60fe6dd8d429ad
cr0barsCorner <58707795+JarredFosbury@users.noreply.github.com>
2024-03-10 04:23:02 -0400

    Added some assets and entities

--- src/game/GameScene.java
+++ src/game/GameScene.java
@@ -1,31 +1,26 @@
 package game;
 
+import engine.core.Camera;
 import engine.core.Entity;
 import engine.core.EntityType;
-import engine.core.KeyListener;
 import engine.core.Scene;
-import engine.fontRendering.Font;
 import engine.fontRendering.FontLoader;
-import engine.rendering.TextMesh;
-import org.joml.Vector2i;
-import org.joml.Vector3f;
-
-import static org.lwjgl.glfw.GLFW.*;
+import engine.rendering.Texture;
 
 public class GameScene extends Entity
 {
     public GameScene()
     {
         super("Game Scene Handler", EntityType.ScriptableBehavior);
-
         Scene.assets.addAssetToPool(FontLoader.loadFont("res/fonts/morris roman/morrisRoman.png",
-                "res/fonts/morris roman/morrisRoman.fnt"), "defaultFont-morrisRoman");
+                "res/fonts/morris roman/morrisRoman.fnt"), "morrisRoman");
+        Scene.assets.addAssetToPool(new Texture("res/textures/fireSheet_lowRes.png", true, false, true), "fireSheet");
+        Scene.assets.addAssetToPool(new Texture("res/textures/campfire.png", true, false, true), "firewood");
 
-        TextMesh titleText = new TextMesh("title-text", (Font) Scene.assets.getAssetFromPool("defaultFont-morrisRoman"), true);
-        titleText.fontSize_PIXELS = 32.0f;
-        titleText.locationAnchor = new Vector2i(0, 0);
-        titleText.textAlignment = new Vector2i(0, 0);
-        titleText.position = new Vector3f(0.0f);
-        titleText.text = "Imagine gameplay here!";
+        Scene.mainCamera = new Camera("Main Camera");
+        Scene.mainCamera.updateViewport(4.0f, -1.0f, 1.0f);
+
+        new FreeCamera();
+        new CampfirePrefab();
     }
 }
