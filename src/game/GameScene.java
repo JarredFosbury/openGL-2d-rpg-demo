c352268f6f539e60556d25965e6a88b4786bcfe4
cr0barsCorner <58707795+JarredFosbury@users.noreply.github.com>
2024-03-12 22:50:14 -0400

    Tested simple lit animated character sprite

--- src/game/GameScene.java
+++ src/game/GameScene.java
@@ -1,15 +1,16 @@
 package game;
 
-import engine.core.Camera;
-import engine.core.Entity;
-import engine.core.EntityType;
-import engine.core.Scene;
+import engine.core.*;
 import engine.fontRendering.FontLoader;
 import engine.rendering.*;
 import org.joml.Vector2f;
+import org.joml.Vector3f;
 
 public class GameScene extends Entity
 {
+    private final MainLightSource source;
+    private final SpriteLit playerSprite;
+
     public GameScene()
     {
         super("Game Scene Handler", EntityType.ScriptableBehavior);
@@ -19,19 +20,28 @@ public class GameScene extends Entity
                 "res/fonts/morris roman/morrisRoman.fnt"), "morrisRoman");
         Scene.assets.addAssetToPool(new Texture("res/textures/litSprites/barrel_alb.png", true, true, true), "spriteAlb");
         Scene.assets.addAssetToPool(new Texture("res/textures/litSprites/barrel_nrm.png", true, true, true), "spriteNrm");
-        Scene.assets.addAssetToPool(new Texture("res/textures/bricks_01.jpg", true, true, true), "bricks");
+        Scene.assets.addAssetToPool(new Texture("res/textures/litSprites/playerJumpRight_alb.png", true, true, true), "playerAlb");
+        Scene.assets.addAssetToPool(new Texture("res/textures/litSprites/playerJumpRight_nrm.png", true, true, true), "playerNrm");
 
         Scene.mainCamera = new Camera("Main Camera");
         Scene.mainCamera.updateViewport(1.5f, -1.0f, 1.0f);
 
-        MainLightSource source = new MainLightSource("mainLightSource", Color.WHITE, 1.0f);
+        source = new MainLightSource("mainLightSource", Color.WHITE, 1.0f);
         source.rotate(-55.0f, 0.0f, -45.0f);
 
         new FreeCamera();
         new SpriteLit("demoSprite-lit", Scene.normalMappedLit2dShader, "spriteAlb",
                 "spriteNrm", Color.WHITE, new Vector2f(0.0f), new Vector2f(1.0f));
+        playerSprite = new SpriteLit("playerSprite-lit", Scene.normalMappedLit2dShader,
+                "playerAlb", "playerNrm", Color.WHITE, new Vector2f(0.0f, 0.857f), new Vector2f(0.1429f));
+        playerSprite.initSpriteSheet("res/spriteSheetData/playerJumpRight_SheetData.ssd");
+        playerSprite.scale = new Vector3f(2.0f);
+        playerSprite.translate(0.0f, 1.450f, 0.0f);
+    }
 
-        Sprite bricks = new Sprite("brickWall",  "bricks", Color.WHITE, new Vector2f(0.0f), new Vector2f(1.0f));
-        bricks.translate(-2.0f, 0.0f, 0.0f);
+    public void update()
+    {
+        source.rotate(0.0f, 0.0f, Time.deltaTime * 45.0f);
+        playerSprite.animateSprite();
     }
 }
